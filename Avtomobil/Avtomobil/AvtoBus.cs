c85d60304b88500e67c239635b9c5678c6fa9ca6
TiKlim <114877716+TiKlim@users.noreply.github.com>
2023-12-19 23:16:13 +0300

    Add files via upload

---
+++ Avtomobil/Avtomobil/AvtoBus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avtomobil
+{
+    internal class AvtoBus : Avto
+    {
+        protected List<Avto> cars = new List<Avto>();
+        protected static void Menu(List<Avto> cars)
+        {
+
+        }
+    }
+}
---
+++ Avtomobil/Avtomobil/Program.cs
@@ -0,0 +1,19 @@
+﻿using Avtomobil;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Avtomobil
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Avto> cars = new List<Avto>();
+            Console.WriteLine("> Доброго времени суток.");
+            Avto.Menu3(cars);
+        }
+    }
+}
+
+
